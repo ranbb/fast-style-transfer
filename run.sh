9b4abf32d4a5fd9c9fd23745915645cb803c4327
Ubuntu <ubuntu@ip-172-31-52-2.ec2.internal>
2019-08-03 21:54:32 +0000

    add changes

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
 #                   OUT_PATH [--device DEVICE] [--batch-size BATCH_SIZE]
 #                   [--allow-different-dimensions]
 mkdir out
-python evaluate.py --checkpoint models/la_muse.ckpt --in-path dl/ --out-path out --allow-different-dimensions --batch-size 1
+for file in dl/*.jpg
+do
+  python evaluate.py --checkpoint models/la_muse.ckpt --in-path $file --out-path out --allow-different-dimensions --batch-size 1
+done
 
